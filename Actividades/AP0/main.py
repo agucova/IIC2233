2ae5dbbdfdae8a39c3d8a8fec7a9fa9aa3b9c67b
Agustín Covarrubias <agucova@uc.cl>
2021-08-26 16:08:39 -0400

    ap0: limpiar archivo y mejorar comentarios 📈

--- Actividades/AP0/main.py
+++ Actividades/AP0/main.py
@@ -1,9 +1,8 @@
-# Debes completar esta función para que retorne la información de los ayudantes
-from os import listxattr
 from typing import Union
 
 
 def cargar_datos(path: str) -> list[list[str]]:
+    """Carga los datos de ayudantes de un archivo y lo retorna como una lista de listas."""
     try:
         with open(path, "r") as ayudantesf:
             ayudantes = [
@@ -17,10 +16,10 @@ def cargar_datos(path: str) -> list[list[str]]:
     return ayudantes
 
 
-# Completa esta función para encontrar la información del ayudante entregado
 def buscar_info_ayudante(
     nombre_ayudante: str, lista_ayudantes: list[list[str]]
 ) -> Union[list[str], None]:
+    """Busca la información de un ayudante por su nombre en una lista dada."""
     assert len(lista_ayudantes[0]) == 4
     nombre_ayudante = nombre_ayudante.lower()
     for ayudante in lista_ayudantes:
@@ -29,26 +28,13 @@ def buscar_info_ayudante(
     return None
 
 
-# Completa esta función para que los ayudnates puedan saludar
 def saludar_ayudante(info_ayudante: list[str]) -> str:
+    """Genera un string de saludo para un ayudante en base a su información."""
     assert len(info_ayudante) == 4
     return f"Hola {info_ayudante[0]}, tu cargo es {info_ayudante[1]} y eres {info_ayudante[2]} en GitHub y {info_ayudante[3]} en Discord."
 
 
 if __name__ == "__main__":
-    pass
-    # El código que aquí escribas se ejecutará solo al llamar a este módulo.
-    # Aquí puedes probar tu código llamando a las funciones definidas.
-
-    # Puede llamar a cargar_datos con el path del archivo 'ayudantes.csv'
-    # para probar si obtiene bien los datos.
-
-    # Puedes intentar buscar la lista de unos de los nombres
-    # que se encuentran en el archivo con la función buscar_info_ayudante.
-    # Además puedes utilizar la lista obtenida para generar su saludo.
-
-    # Hint: la función print puede se útil para revisar
-    #       lo que se está retornando.
-    ayudantes = cargar_datos("Actividades/AP0/ayudanddtes.csv")
+    ayudantes = cargar_datos("Actividades/AP0/ayudantes.csv")
     ayudante = buscar_info_ayudante("Francisca Ibarra", ayudantes)
     print(saludar_ayudante(ayudante))
