242d057dbf99b560c91a1dfb7ecfa6afecbc3384
Agustín Covarrubias <agucova@uc.cl>
2021-09-02 16:11:35 -0400

    af1: mejor formato para los prints

--- Actividades/AF1/mascota.py
+++ Actividades/AF1/mascota.py
@@ -69,7 +69,7 @@ class Mascota:
         self.saciedad += p.SACIEDAD_PASEAR
 
     def __str__(self):
-        return f"{self.nombre} - {self.especie} ({self.raza})\nSAC: {self.saciedad}\nENT: {self.entretencion}\nSAT: {self.saciedad}"
+        return f"{self.nombre} - {self.especie} ({self.raza})\nSAC: {self.saciedad}\nENT: {self.entretencion}\nSAT: {self.saciedad}\n"
 
 
 class Perro(Mascota):
