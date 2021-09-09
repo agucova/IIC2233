5f9882048ffd05fb9dffa2beb3a2bc90541963a5
Agustín Covarrubias <agucova@uc.cl>
2021-09-09 15:07:16 -0300

    as1: implement Atracciones

--- Actividades/AS1/atracciones.py
+++ Actividades/AS1/atracciones.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import Union
 import parametros as p
+from personas import Adulto, Nino
 
 
 # Recuerda definir esta clase como abstracta!
@@ -47,43 +50,62 @@ class Atraccion(ABC):
 
 # Recuerda completar la herencia!
 class AtraccionFamiliar(Atraccion):
-    def __init__(self):
-        # COMPLETAR
-        pass
+    def __init__(self, nombre: str, capacidad: int):
+        super().__init__(nombre, capacidad)
+        self.efecto_salud: int = p.SALUD_FAMILIA
+        self.efecto_felicidad: int = p.FELICIDAD_FAMILIA
 
     def efecto_atraccion(self, persona):
-        # COMPLETAR
-        pass
+        persona.felicidad += self.efecto_felicidad
+        persona.salud -= self.efecto_salud
 
 
 # Recuerda completar la herencia!
 class AtraccionAdrenalinica(Atraccion):
-    def __init__(self):
-        # COMPLETAR
-        pass
+    def __init__(self, nombre: str, capacidad: int, salud_necesaria: int):
+        super().__init__(nombre, capacidad)
+        self.salud_necesaria: int = salud_necesaria
+        self.efecto_salud: int = p.SALUD_ADRENALINA
+        self.efecto_felicidad: int = p.FELICIDAD_ADRENALINA
 
     def efecto_atraccion(self, persona):
-        # COMPLETAR
-        pass
+        if persona.salud > self.salud_necesaria:
+            persona.felicidad += self.efecto_felicidad
+            persona.salud -= self.efecto_salud
+        else:
+            print(f"*** {persona.nombre} fue bajado del juego por su salud")
+            persona.salud -= p.SALUD_ADRENALINA // 2
+            persona.felicidad -= p.FELICIDAD_ADRENALINA // 2
 
 
 # Recuerda completar la herencia!
-class AtraccionAcuatica(Atraccion):
-    def __init__(self):
-        # COMPLETAR
-        pass
 
-    def ingresar_persona(self, persona):
-        # COMPLETAR
-        pass
+
+class AtraccionAcuatica(AtraccionFamiliar):
+    def __init__(self, nombre, capacidad):
+        super().__init__(nombre, capacidad)
+        self.efecto_felicidad = p.FELICIDAD_ACUATICA
+
+    def ingresar_persona(self, persona: Union[Nino, Adulto]):
+        if persona.tiene_pase:
+            super().ingresar_persona(persona)
+        else:
+            print(f"*** {persona.nombre} no puede entrar sin su pase de movilidad.")
 
 
 # Recuerda completar la herencia!
-class MontanaAcuatica(Atraccion):
-    def __init__(self):
-        # COMPLETAR
-        pass
+class MontanaAcuatica(AtraccionAdrenalinica, AtraccionAcuatica):
+    def __init__(
+        self, nombre: str, capacidad: int, salud_necesaria: int, dificultad: int
+    ):
+        super().__init__(nombre, capacidad, salud_necesaria)
+        self.dificultad: int = dificultad
 
-    def iniciar_juego(self, personas):
-        # COMPLETAR
-        pass
+    def iniciar_juego(self, personas: list[Union[Nino, Adulto]]):
+        for persona in personas:
+            print(f"*** {persona.nombre} ha jugado en {self.nombre}")
+            persona.esperando = False
+            if persona.salud <= self.salud_necesaria * self.dificultad:
+                print(f"*** {persona.nombre} se cayó del agua y perdió su pase")
+                persona.tiene_pase = False
+            self.efecto_atraccion(persona)
