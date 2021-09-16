a618079b0dd09333ca76f6ebe59d7172757a430e
Agustín Covarrubias <agucova@uc.cl>
2021-09-16 15:01:17 -0300

    af2: implementar permitir_entrada

--- Actividades/AF2/permitir_entrada.py
+++ Actividades/AF2/permitir_entrada.py
@@ -1,3 +1,5 @@
+from Actividades.AF2.invitades import Invitade
+from __future__ import annotations
 from excepciones_covid import RiesgoCovid
 
 
@@ -11,6 +13,14 @@ def verificar_sintomas(invitade):
         raise RiesgoCovid("dolor_cabeza", invitade.nombre)
 
 
-def entregar_invitados(diccionario_invitades):
-    # Completar
-    pass
+def entregar_invitados(diccionario_invitades: dict[str, Invitade]) -> list[Invitade]:
+    invitades_sin_riesgo: list[Invitade] = []
+    for invitade in diccionario_invitades.values():
+        try:
+            verificar_sintomas(invitade)
+        except RiesgoCovid as riesgo:
+            riesgo.alerta_de_covid()
+        else:
+            invitades_sin_riesgo.append(invitade)
+
+    return invitades_sin_riesgo
