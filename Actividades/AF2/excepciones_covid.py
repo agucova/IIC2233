99c2deea2fef9dad791c5a0804ef40e44c3fd0ff
Agustín Covarrubias <agucova@uc.cl>
2021-09-16 15:01:03 -0300

    af2: implementar RiesgoCovid

--- Actividades/AF2/excepciones_covid.py
+++ Actividades/AF2/excepciones_covid.py
@@ -1,8 +1,18 @@
-class RiesgoCovid:
-    def __init__(self, sintoma, nombre_invitade):
-        # Completar
-        pass
+class RiesgoCovid(Exception):
+    def __init__(self, sintoma: str, nombre_invitade: str):
+        self.sintoma = sintoma
+        self.nombre_invitade = nombre_invitade
 
     def alerta_de_covid(self):
-        # Completar
-        pass
+        if self.sintoma == "fiebre":
+            print(
+                f"La persona {self.nombre_invitade} tiene fiebre y tiene prohibido el ingreso"
+            )
+        elif self.sintoma == "dolor_cabeza":
+            print(
+                f"La persona {self.nombre_invitade} tiene dolor de cabeza y tiene prohibido el ingreso"
+            )
+        elif self.sintoma == "tos":
+            print(
+                f"La persona {self.nombre_invitade} tiene tos y tiene prohibido el ingreso"
+            )
