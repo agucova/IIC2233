ddb079d9af143778fd4eb1c958bf531f484b6f35
Agustín Covarrubias <agucova@uc.cl>
2021-09-23 15:43:09 -0300

    as2: implementar App

--- Actividades/AS2/app.py
+++ Actividades/AS2/app.py
@@ -1,12 +1,19 @@
+from __future__ import annotations
 from random import randint
 from time import sleep
 from pedido import Pedido
 from shopper import Shopper
+from tienda import Tienda
 from threading import Thread
 
 
-class DCComidApp:
-    def __init__(self, shoppers, tiendas, pedidos):
+class DCComidApp(Thread):
+    def __init__(
+        self,
+        shoppers: list[Shopper],
+        tiendas: list[Tienda],
+        pedidos: list[tuple[str, str, str]],
+    ):
         # NO MODIFICAR
         super().__init__()
         self.shoppers = shoppers
@@ -14,12 +21,29 @@ class DCComidApp:
         self.tiendas = tiendas
 
     def obtener_shopper(self):
-        # Completar
-        pass
+        while True:
+            for shopper in self.shoppers:
+                if not shopper.ocupado:
+                    return shopper
+            print("Todos los shoppers están ocupados, esperando disponibilidad.")
+            Shopper.evento_disponible.wait()
 
     def run(self):
-        # Completar
-        pass
+        while self.pedidos:
+            id_, nombre_tienda, descripcion = self.pedidos.pop(0)
+            q_tiendas: list[Tienda] = [
+                tienda for tienda in self.tiendas if tienda.nombre == nombre_tienda
+            ]
+            assert len(q_tiendas) == 1
+            tienda: Tienda = q_tiendas[0]
+
+            pedido = Pedido(id_, nombre_tienda, descripcion)
+
+            shopper: Shopper = self.obtener_shopper()
+            shopper.asignar_pedido(pedido)
+            tienda.ingresar_pedido(pedido, shopper)
+            trafico_de_red = randint(1, 5)
+            sleep(trafico_de_red)
 
 
 if __name__ == "__main__":
