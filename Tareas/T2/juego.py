530d84602a565a59fa69277b7280e6efc64dee9c
Agustín Covarrubias <agucova@uc.cl>
2021-11-12 21:49:40 -0300

    t2: significant progress on drawing canvas

--- Tareas/T2/juego.py
+++ Tareas/T2/juego.py
@@ -1,6 +1,9 @@
-from dataclasses import dataclass
-import parametros as p
 from __future__ import annotations
+
+import parametros as p
+from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QObject, pyqtSignal
+from random import choice
 
 
 class Froggy:
@@ -22,12 +25,21 @@ class Froggy:
             self.posicion = (self.posicion[0] + 1, self.posicion[1])
 
 
-class Auto:
-    def __init__(self):
-        self.posicion = (0, 0)
+class Game:
+    def __init__(self) -> None:
+        self.froggy: Froggy = None
 
 
-class Carretera:
+class RoadGame(QObject):
+    paint_car_signal = pyqtSignal(str)
+
     def __init__(self):
-        self.periodo_autos = p.PERIODO_AUTOS
-        self.autos: list[Auto] = []
+        super().__init__()
+        self.car_spawn_period = p.PERIODO_AUTOS * 1000
+        self.car_spawner = QTimer()
+        self.car_spawner.timeout.connect(self.spawn_car)
+        self.car_spawner.start(self.car_spawn_period)
+
+    def spawn_car(self):
+        print(f"[DEBUG] Spawned car.")
+        self.paint_car_signal.emit(choice(("left", "right")))
--- Tareas/T2/main.py
+++ Tareas/T2/main.py
@@ -1,6 +1,19 @@
 import sys
+
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QBrush, QPainter, QPen, QPixmap
+from PyQt5.QtWidgets import (
+    QApplication,
+    QGraphicsItem,
+    QGraphicsScene,
+    QGraphicsView,
+    QMainWindow,
+    QWidget,
+    QGraphicsPixmapItem,
+)
+
+from juego import RoadGame
 
 
 class VentanaInicio(QMainWindow):
@@ -19,8 +32,51 @@ class VentanaInicio(QMainWindow):
 class VentanaJuego(QMainWindow):
     def __init__(self, usuario):
         super(VentanaJuego, self).__init__()
+        self.init_ui()
+        self.init_road()
+
+    def init_ui(self):
         uic.loadUi("ventanas/juego.ui", self)
+
+        self.scene_x = 30
+        self.scene_y = 350
+        self.scene_width = 1240
+        self.scene_height = 500
+
+        self.scene = QGraphicsScene(
+            self.scene_x, self.scene_y, self.scene_width, self.scene_height
+        )
+
+        view = QGraphicsView(self.scene, self)
+        view.setRenderHint(QPainter.Antialiasing)
+
+        self.layout_view.addWidget(view)
+
         self.show()
+
+    def paint_scene_background(self, path):
+        """Scales and adds the given background to the Graphics Scene."""
+        background = QGraphicsPixmapItem(
+            QPixmap(path).scaled(self.scene_width, self.scene_height)
+        )
+        background.setOffset(self.scene_x, self.scene_y)
+        self.scene.addItem(background)
+
+    ###########################
+    # Roads Game (Carreteras) #
+    ###########################
+    def init_road(self):
+        self.scene.clear()
+        self.paint_scene_background("sprites/Mapa/areas/carretera.png")
+
+        self.game = RoadGame()
+        self.game.paint_car_signal.connect(self.spawn_car)
+
+    def spawn_car(self, side):
+        assert side in ("left", "right")
+        car = QGraphicsPixmapItem(QPixmap("sprites/Mapa/autos/morado_left.png"))
+        car.setOffset(self.scene_x + 20, self.scene_y + 20)
+        self.scene.addItem(car)
 
 
 class VentanaRanking(QMainWindow):
